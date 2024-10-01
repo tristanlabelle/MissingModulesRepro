e908e129aed8f4d184b684019f2e7eb377d7d930
Tristan Labelle <git@tristanlabelle.com>
2024-10-01 05:39:19 -0400

    Gutted COM

---
+++ Support/COM/Imports.swift
@@ -0,0 +1,2 @@
+import SWRT_SwiftCOMObject
+import ABIBindings
--- Support/COM/comIdentical.swift
+++
@@ -1,5 +0,0 @@
-public func comIdentical(_ lhs: IUnknown, _ rhs: IUnknown) -> Bool {
-    guard let lhsIdentityReference = try? lhs._queryInterface(IUnknownBinding.interfaceID) else { return false }
-    guard let rhsIdentityReference = try? rhs._queryInterface(IUnknownBinding.interfaceID) else { return false }
-    return lhsIdentityReference.pointer == rhsIdentityReference.pointer
-}
