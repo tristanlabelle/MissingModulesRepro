a94b059e0bb6afa0f32d72cde5997ca010512080
Tristan Labelle <git@tristanlabelle.com>
2024-10-01 05:44:22 -0400

    Gutted ABIBindings

--- Support/ABIBindings/IdentityBinding.swift
+++
@@ -1,7 +0,0 @@
-/// Binds a Swift type to an identical ABI type.
-public protocol IdentityBinding: PODBinding where SwiftValue == ABIValue {}
-
-extension IdentityBinding {
-    public static func toABI(_ value: SwiftValue) -> ABIValue { value }
-    public static func fromABI(_ value: ABIValue) -> SwiftValue { value }
-}
